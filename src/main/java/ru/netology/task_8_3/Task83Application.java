48cdf80bd9a2227aaa4af47d0accf628de132091
Andrew Boiko <boiko_andrew@mail.ru>
2024-01-15 01:22:22 +0300

    Initial commit

---
+++ src/main/java/ru/netology/task_8_3/Task83Application.java
@@ -0,0 +1,13 @@
+package ru.netology.task_8_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task83Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task83Application.class, args);
+    }
+
+}
---
+++ src/main/java/ru/netology/task_8_3/model/Authorities.java
@@ -0,0 +1,5 @@
+package ru.netology.task_8_3.model;
+
+public enum Authorities {
+    READ, WRITE, DELETE
+}
